970105ea30da7303732b71f95f1de7c58844c852
JermPlayz <jeremycschoonover@icloud.com>
2025-01-14 11:01:41 -0600

    push

---
+++ kill_zone.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func posX():
+	return $Level.posX
+
+func poxY():
+	return $Player.posY()
--- level.gd
+++ level.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-func _physics_process(delta) -> void:
-	$KillZone.position.y = $Player.position.y
-	$KillZone.position.x += $Player.SPEED*delta
+func _physics_process(delta):
+	var posY = $Player.posY()
+	var posX = $Player.SPEED*delta
 	
 	
 
 
-func _on_kill_zone_body_entered(body: Node2D) -> void:
+func _on_kill_zone_body_entered(body: Node2D):
 	var things = $KillZone.get_overlapping_bodies()
 	for t in things:
 		if t.has_method("Explode"):
