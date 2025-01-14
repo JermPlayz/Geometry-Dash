09bf211d52537cf4f1c032c4f0795411c7ec4536
JermPlayz <jeremycschoonover@icloud.com>
2025-01-14 10:19:17 -0600

    bruh

---
+++ level.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _physics_process(delta) -> void:
+	$KillZone.position.y = $Player.position.y
+	$KillZone.position.x += $Player.SPEED*delta
+	
+	
+
+
+func _on_kill_zone_body_entered(body: Node2D) -> void:
+	var things = $KillZone.get_overlapping_bodies()
+	for t in things:
+		if t.has_method("Explode"):
+			t.Explode()
+			$KillZone.queue_free()
