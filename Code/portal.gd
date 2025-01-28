cc286cf882bb386c5d2bfb913696094b9b9c0ff4
JermPlayz <jeremycschoonover@icloud.com>
2025-01-28 10:12:54 -0600

    Bunch of sprites

--- Code/portal.gd
+++ Code/portal.gd
@@ -7,7 +7,5 @@ func _ready():
 	match type :
 		0 : #CUBE
 			sprite.texture = load("res://Sprites/CubePortalLabelled.webp")
-		1 : #SHIP
-			sprite.texture = load("res://Sprites/ShipPortalLabelled.webp")
-		2 : #UFO
-			sprite.texture = load("res://Sprites/UFOPortalLabelled.webp")
+		1 : #WAVE
+			sprite.texture = load("res://Sprites/WavePortalLabelled.webp")
