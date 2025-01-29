3236e40bda63e9801efd0263a478b5dd9784bb6a
JermPlayz <jeremycschoonover@icloud.com>
2025-01-29 10:26:24 -0600

    push all this stuff

--- Code/portal.gd
+++ Code/portal.gd
@@ -9,11 +9,3 @@ func _ready():
 			sprite.texture = load("res://Sprites/CubePortalLabelled.webp")
 		1 : #WAVE
 			sprite.texture = load("res://Sprites/WavePortalLabelled.webp")
-		2 : #BALL
-			sprite.texture = load("res://Sprites/BallPortalLabelled.webp")
-		3 : #UFO
-			sprite.texture = load("res://Sprites/UFOPortalLabelled.webp")
-		4 : #UP
-			sprite.texture = load("res://Sprites/GravityPortalALabelled.webp")
-		5 : #DOWN
-			sprite.texture = load("res://Sprites/GravityPortalBLabelled.webp")
